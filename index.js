16fe7703c5d228feeadea41c5155fb52f546116d
Julian Jimenez <julian.j.jimenez@student.uncommonhs.org>
2017-02-08 22:25:53 +0000

    I changed Jquery

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 $(document).ready( function(){
     $('#change-color').click(function(){
         $('body').css("background-color","red");
-    })
-})
+    });
+    
+    
+    $('#submit-name').click(function() {
+        var name=$('#name').val();
+        $('#greeting').html("Hi "+ $('#name').val());
+    });
+    
+    $('.gallery-thumb').click(function() {
+        $('#gallery-main').attr('src','#gallery-thumb')
+    }
+
+
+    
+    
+
+    
+    
+    
+});
 
